77c86d3fab7ba116eb9839e9fb1b94cf1781ace7
可爱的魏氏大萌神 <hkgjwtl2017@aliyun.com>
2023-11-09 15:44:29 +0800

    可爱的魏氏大萌神  15:37:06 乐乐 is crushed
    可爱的魏氏大萌神  15:37:34
    please check the data log for further information（return code=NULL)

--- 回调函数/main.c
+++ 回调函数/main.c
@@ -25,6 +25,7 @@ typedef struct stu {
 }stu;
 
 
+
 int compare_studentage(const void* p1, const void* p2) {//为了迎合库函数的参数列表
     //if (((stu*)p1)->age <  ((stu*)p2)->age) {//但是实际上还要强制转换成正确的样子
     //    return -2;
@@ -39,25 +40,50 @@ int compare_studentage(const void* p1, const void* p2) {//为了迎合库函数的参数列
     //其实也可以作差
     return (((stu*)p1)->age - ((stu*)p2)->age);//整数做差没问题
 }
+typedef int(*compare_func)(const void*, const void*);
 
+void Swap(char* buf1, char* buf2,int width) {
+    int i = 0;
+    for (; i < width; i++) {
+        char tmp = *(buf1+i);
+        *(buf1+i) = *(buf2+i);
+        *(buf2 + i) = tmp;
+    }
+    return;
+}
 
+void bubble(void* base, int size, int width,compare_func compare_func) {
+    int flag = 1,i=0;
+    while (flag) {
+        flag = 0;
+        for (i = 0; i < size; i++) {
+            if (compare_func(((char*)base + i * width), ((char*)base + (i + 1) * width))>0) {
+                //void不能直接使用(0个字节)，先强制赋char*，让它变成一个字节；然后+i*width来定位到待比较对象的开始地址
+                //交换
+                flag = 1;
+                Swap((char*)base + i * width, (char*)base + (i + 1) * width, width);
+            
+            }
+        }
+    }
+}
 
 int main(void) {
-    
-    double arr1[10] = {
+    int i = 0;
+    /*double arr1[10] = {
         1.0,3.0,5.0,2.2345246,4.0,6.0,9.0,5.6,5.3,8.2
     };
     qsort(arr1, sizeof(arr1) / sizeof(arr1[0]), sizeof(double), compare_double);
-    int i = 0;
+    
     for (; i < sizeof(arr1) / sizeof(arr1[0]); i++) {
         printf("%lf\n", arr1[i]);
-    }
+    }*/
 
     stu arr_stu[3] = {
         {"张三",24},{"李斯",10},{"王五",13}
     };
 
-    qsort(arr_stu, sizeof(arr_stu) / sizeof(arr_stu[0]), sizeof(stu), compare_studentage);
+    bubble(arr_stu, sizeof(arr_stu) / sizeof(arr_stu[0]), sizeof(stu), compare_studentage);
     for (i = 0; i < sizeof(arr_stu) / sizeof(arr_stu[0]); i++) {
         printf("第%d位，姓名%s，年龄%d\n", i, arr_stu[i].name, arr_stu[i].age);
     }
